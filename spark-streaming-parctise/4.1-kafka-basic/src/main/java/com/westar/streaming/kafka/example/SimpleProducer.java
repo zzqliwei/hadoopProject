44abbd194c462512f1192d7e26b2363a85c68a66
zzqliwei <1209114537@qq.com>
2020-01-17 11:41:50 +0800

    kafka使用方式Java案例

---
+++ spark-streaming-parctise/4.1-kafka-basic/src/main/java/com/westar/streaming/kafka/example/SimpleProducer.java
@@ -0,0 +1,24 @@
+package com.westar.streaming.kafka.example;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class SimpleProducer {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "master:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("batch.size", "10");
+
+        KafkaProducer<String,String> producer = new KafkaProducer<String, String>(props);
+        for (int i = 0; i < 100; i++) {
+            producer.send(new ProducerRecord<String, String>("test-group",
+                    Integer.toString(i), Integer.toString(i)));
+        }
+
+        producer.close();
+    }
+}
